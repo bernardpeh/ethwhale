76be02079f6967eb6fda0ac8ef47e4a5bd89b129
bernard peh <bpeh777@gmail.com>
2018-03-15 23:20:40 +1100

    update sample script

--- scripts/import_erc20_contrib.js
+++ scripts/import_erc20_contrib.js
@@ -4,6 +4,21 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("MYSQL Connected!");
   // var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  var options = {
+    fromBlock: "latest"-1,
+    toBlock: "latest",
+    // address: "0xf1f64f6b8e17dd68c1db10b0eed3d2541a6c09ab"
+    topics: [web3.sha3("Transfer(address,address,uint256)")]
+  };
+
+  var filter = web3.eth.filter(options);
+
+  filter.get(function(error, result) {
+    if (!error) {
+      console.log(result);
+    }
+  });
+
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
